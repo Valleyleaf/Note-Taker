abf178fb5c7b32091684fd9c05831675692bbf35
Aston Dahllöf <aston_dahllof@outlook.com>
2023-10-12 17:45:38 -0300

    Added function. Moving to desktop

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const express = require('express')
 const app = express()
 
 app.listen(3000)
+
+app.get('./', (req, res) =>{
+    console.log('Confirmed line 9')
+    res.sendfILE(path.join(__dirname, 'index.html'))
+});
