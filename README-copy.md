94898af16dace19427e79876e17f4b18a3795f56
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-14 14:55:01 -0300

    Got notes path to work. Next step is db.json functionality.

--- README-copy.md
+++ README-copy.md
@@ -19,10 +19,10 @@ SO THAT I can organize my thoughts and keep track of tasks I need to complete
 ## Acceptance Criteria
 
 ```
-GIVEN a note-taking application
-WHEN I open the Note Taker
-THEN I am presented with a landing page with a link to a notes page
-WHEN I click on the link to the notes page
+GIVEN a note-taking application DONE
+WHEN I open the Note Taker DONE
+THEN I am presented with a landing page with a link to a notes page DONE
+WHEN I click on the link to the notes page DONE
 THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 WHEN I enter a new note title and the note’s text
 THEN a Save icon appears in the navigation at the top of the page
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const noteData = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
+// Used this video for reference: https://www.youtube.com/watch?v=ENrzD9HAZK4&t=653s
 
 app.use(express.static(path.join(__dirname, 'Develop/public')));
 
@@ -11,25 +13,22 @@ app.use(express.json());
 //Above parses JSON request. Dig into how this works more.
 
 app.get('/', (req, res) =>{
-    console.log('hit index.js')
+    console.log('hit index.html')
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
-//Above get request calls for 
 
-app.get('/api/notes', (req, res) =>{
-    console.log('hit db.json')
-    fs.sendFile(path.join(__dirname, '/Develop/db/db.json'), (err, data) => {
-        if (err) throw error
-        return res.json(JSON.parse(data));
-})});
-// Get is retreving items. If the function can't grab the file. It will crash and provide an error in the console.
-
-app.post('/', (req, res) =>{
-    fs.writeFile(path.join(__dirname, 'index.html'));
+app.get('/notes', (req, res) =>{
+    console.log('hit notes.html')
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
-// post is creating items. Req body is needed.
 
-// :id
+// Why did we name this api. Button does not bring us here. Modify html asset?
+// app.get('/notes', (req, res) => res.json(noteData));
+
+// Learning note: You need to handle both front-end and back-end when dealing with back-end that has visuals outside the console. First get displays html.
+// second get sets up the back-end.
+
+
 app.delete('/', (req, res) =>{
     // We want to read from db. Using fs.readFile(Path to db).
     // if/else if we get data or if we get nothing.
@@ -41,4 +40,4 @@ app.delete('/', (req, res) =>{
 //Get Push, Post and Delete are concepts for you to dig into.
 
 // Use listen at the bottom. It is simular to a return in that sense.
-app.listen(3001);
+app.listen(PORT, () => console.log('Available on PORT 3001'));
