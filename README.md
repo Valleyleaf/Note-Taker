2987a98b1da253439a0f9377f27051fe53ee9090
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-10-18 22:11:45 -0300

    Finalized function to save notes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Note Taker Starter Code
+
+## 📝licence
+![Github license](https://img.shields.io/badge/license-MIT,-green.svg)
+
+## ✨Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Dependancies](#Dependancies)
+* [Usage](#Usage)
+* [licence](#licence)
+* [Questions](#Questions)
+
+## 💻Description
+This is a Note-Taker project, designed to learn how to connect Front-End programing to the back end. The Front-End is pre-made, but the Back-End is custom and created by me.
+
+## 🚨Installation
+Download and run the server.js with the node command or deploy using Heroku.
+
+## ✅Dependancies
+This depends on Express.js, Node.js and Heroku.
+
+## 🚀Usage
+Launch application with node or Heroku. Open a browser of choice and navigate to http://localhost:3001/. Use the buttons to create and save notes.
+
+## 💊Test
+N/A
+
+## 👤Questions
+Find me at https://github.com/Valleyleaf or reach out to me
+at: valleyleaf@gmail.com
+
--- server.js
+++ server.js
@@ -1,51 +1,62 @@
-//Const Library goes here
 const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const noteData = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 // Used this video for reference: https://www.youtube.com/watch?v=ENrzD9HAZK4&t=653s
 
-app.use(express.static(path.join(__dirname, 'Develop/public')));
+app.use(express.static(path.join(__dirname, '/Develop/public')));
 
 app.use(express.json());
 //Above parses JSON request. Dig into how this works more.
 
-
-// Below are the two get requests for pulling index and notes .html. I also added error responses to be fancy.
-app.get('/', (req, res) =>{
-    try {
-        res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
-
-    } catch (err) {
-        response.status(500).send('No response from server.')
-    }
-});
-
 app.get('/notes', (req, res) =>{
-    try {
-        res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
-
-    } catch (err) {
-        response.status(500).send('No response from server.')
-    }
+        res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json', (err, data) => {
+        err ? console.log('error on 23'):res.json(JSON.parse(data));
+    })
+})
 
 // Learning note: You need to handle both front-end and back-end when dealing with back-end that has visuals outside the console. First get displays html.
 // second get sets up the back-end. //New note. Is the above true? potentially but not an absolute.
+// Post is create
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json', (err, data) => {
+        if (err){
+            res.status(500).json('Error occurred while saving.');
+        }else{
+            let allNotes = JSON.parse(data)
+            let newNote = {
+                title:req.body.title,
+                text:req.body.text
+            }
+            allNotes.push(newNote)
+            fs.writeFile('./Develop/db/db.json', JSON.stringify(allNotes, null, 4), (err) => {
+                if (err) {
+                    res.status(500).json('Error occurred while saving.');
+                } else {
+                  res.json('Its been saved');
+                }
+              });
+        }
+    })
+})
 
 
+// Using post to express to api/notes. Need to figure out what api/notes refers to. Probably in index.js
+// Can't get this to work for some reason. I need help with fileSystem. Ask Kevin/Toua.
 
 app.delete('/', (req, res) =>{
+    // Do this if I have extra time.
     // We want to read from db. Using fs.readFile(Path to db).
     // if/else if we get data or if we get nothing.
     // if no error, parse and return data using JSON.
     // write logic to delete returned data.
     // writeFile update to db removing the deleted data.
 });
-
-//Get Push, Post and Delete are concepts for you to dig into.
 
 // Use listen at the bottom. It is simular to a return in that sense.
 app.listen(PORT, () => console.log('Available on PORT 3001'));
